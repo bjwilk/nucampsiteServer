f89814441c24d5776ddf2f040435a7f991682b41
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2023-05-23 19:30:58 -0700

    updated week 4

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
-const favoriteRouter = require('./routes/favoriteRouter')
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
